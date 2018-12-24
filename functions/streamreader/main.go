d57203195ba4ed72573a55c09de1733c64873c43
d-smith <doug.smith.mail@gmail.com>
2018-12-24 09:17:48 -0800

    Added publish to queue

--- functions/streamreader/main.go
+++ functions/streamreader/main.go
@@ -4,19 +4,28 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 //https://play.golang.org/p/955Nm7AYuWu
 
 var div = new(big.Int)
 var bigZero = new(big.Int)
+var snsSvc *sns.SNS
+var topicArn *string
 
 func init() {
 	div.SetInt64(5)
 	bigZero.SetInt64(0)
+	session := session.Must(session.NewSession())
+	snsSvc = sns.New(session)
+	topicArn = aws.String(os.Getenv("TOPIC"))
 }
 
 func processIt(sequenceNo string) bool {
@@ -24,6 +33,21 @@ func processIt(sequenceNo string) bool {
 	i.SetString(sequenceNo, 10)
 	m1 := new(big.Int).Mod(i, div)
 	return m1.Cmp(bigZero) != 0
+}
+
+func sendIt(data string) error {
+	fmt.Printf("send %s\n", data)
+
+	pubIn := sns.PublishInput{
+		Message:  aws.String(data),
+		TopicArn: topicArn,
+	}
+
+	pubOut, err := snsSvc.Publish(&pubIn)
+	if err != nil {
+		fmt.Println(pubOut)
+	}
+	return err
 }
 
 func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
@@ -39,6 +63,9 @@ func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
 		}
 
 		fmt.Printf("Process record %+v\n", kinesisRecord)
+		if err := sendIt(string(kinesisRecord.Data)); err != nil {
+			fmt.Println("Error publishing:", err)
+		}
 	}
 
 	return nil
